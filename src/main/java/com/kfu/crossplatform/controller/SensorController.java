d27f247ed47f9e892f561268130ee6ae21ded675
nmsurv <gurlev356@gmail.com>
2026-01-19 14:56:47 +0300

    Реализованы CRUD опреации для объектов Sensor в SensorController и SensorService

--- src/main/java/com/kfu/crossplatform/controller/SensorController.java
+++ src/main/java/com/kfu/crossplatform/controller/SensorController.java
@@ -14,14 +14,34 @@ public class SensorController {
 
     private final SensorService sensorService;
 
+    // CREATE
     @PostMapping
     public Sensor create(@RequestBody Sensor sensor) {
         return sensorService.create(sensor);
     }
 
-
+    // READ ALL
     @GetMapping
     public List<Sensor> findAll() {
         return sensorService.findAll();
+    }
+
+    // READ BY ID
+    @GetMapping("/{id}")
+    public Sensor findById(@PathVariable Long id) {
+        return sensorService.findById(id);
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public Sensor update(@PathVariable Long id,
+                          @RequestBody Sensor sensor) {
+        return sensorService.update(id, sensor);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        sensorService.delete(id);
     }
 }
--- src/main/java/com/kfu/crossplatform/service/SensorService.java
+++ src/main/java/com/kfu/crossplatform/service/SensorService.java
@@ -13,11 +13,35 @@ public class SensorService {
 
     private final SensorRepository sensorRepository;
 
+    // CREATE
     public Sensor create(Sensor sensor) {
+        sensor.setId(null); // БД сама сгенерирует id
         return sensorRepository.save(sensor);
     }
 
+    // READ ALL
     public List<Sensor> findAll() {
         return sensorRepository.findAll();
+    }
+
+    // READ BY ID
+    public Sensor findById(Long id) {
+        return sensorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Sensor id = " + id + " не найден."));
+    }
+
+    // UPDATE
+    public Sensor update(Long id, Sensor newSensor) {
+        Sensor existing = findById(id);
+
+        existing.setModel(newSensor.getModel());
+        existing.setLocation(newSensor.getLocation());
+
+        return sensorRepository.save(existing);
+    }
+
+    // DELETE
+    public void delete(Long id) {
+        sensorRepository.deleteById(id);
     }
 }
